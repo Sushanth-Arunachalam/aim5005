c7d85a38de6ad13842da868fb5d40c23e69858ce
Sushanth-Arunachalam <60148115+Sushanth-Arunachalam@users.noreply.github.com>
2025-02-18 21:53:19 -0500

    Add files via upload

---
+++ features.py
@@ -0,0 +1,106 @@
+import numpy as np
+from typing import List, Tuple
+### YOU MANY NOT ADD ANY MORE IMPORTS (you may add more typing imports)
+
+class MinMaxScaler:
+    def __init__(self):
+        self.minimum = None
+        self.maximum = None
+        
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+        
+    
+    def fit(self, x:np.ndarray) -> None:   
+        x = self._check_is_array(x)
+        self.minimum=x.min(axis=0)
+        self.maximum=x.max(axis=0)
+        
+    def transform(self, x:np.ndarray) -> list:
+        """
+        MinMax Scale the given vector
+        """
+        x = self._check_is_array(x)
+        diff_max_min = self.maximum - self.minimum
+        
+        # TODO: There is a bug here... Look carefully! 
+        return (x-self.minimum)/(self.maximum-self.minimum)
+    
+    def fit_transform(self, x:list) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
+    
+    
+class StandardScaler:
+    def __init__(self):
+        self.mean = None
+        self.std=None 
+
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+        
+    
+    def fit(self, x:np.ndarray) -> None:   
+        x = self._check_is_array(x)
+        self.mean=x.mean(axis=0)
+        self.std=x.std(axis=0)
+        
+    def transform(self, x:np.ndarray) -> list:
+        """
+        MinMax Scale the given vector
+        """
+        x = self._check_is_array(x)
+        new_std = np.where(self.std == 0, 1, self.std)
+        
+        # TODO: There is a bug here... Look carefully! 
+        return (x - self.mean) / new_std
+    
+    def fit_transform(self, x:list) -> np.ndarray:
+        x = self._check_is_array(x)
+        return self.self.fit(x).transform(x)
+
+
+class LabelEncoder:
+
+    def __init__(self):
+        self.classes_ = None
+    
+    def _check_is_array(self, x) -> np.ndarray:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+        assert isinstance(x, np.ndarray), "Expected the input to be a numpy array"
+        return x
+        
+    def fit(self, x: List) -> None:
+        x = self._check_is_array(x)
+        self.classes_ = np.unique(x)
+        
+    def transform(self, x: List) -> np.ndarray:
+        x = self._check_is_array(x)
+        if self.classes_ is None:
+            raise ValueError("LabelEncoder does not have any labels defined")
+        
+        for labels in x:
+            if labels not in self.classes_:
+                raise ValueError(f"Unknown label -> '{labels}' found in labelencoder() class. try fitting the encoder again.")
+        
+        return np.array([np.where(self.classes_ == val)[0][0] for val in x])
+    
+    def fit_transform(self, x: List) -> np.ndarray:
+        self.fit(x)
+        return self.transform(x)
---
+++ test_features.py
@@ -0,0 +1,96 @@
+from aim5005.features import MinMaxScaler, StandardScaler, LabelEncoder
+import numpy as np
+import unittest
+from unittest.case import TestCase
+
+### TO NOT MODIFY EXISTING TESTS
+
+class TestFeatures(TestCase):
+    def test_initialize_min_max_scaler(self):
+        scaler = MinMaxScaler()
+        assert isinstance(scaler, MinMaxScaler), "scaler is not a MinMaxScaler object"
+        
+        
+    def test_min_max_fit(self):
+        scaler = MinMaxScaler()
+        data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
+        scaler.fit(data)
+        assert (scaler.maximum == np.array([1., 18.])).all(), "scaler fit does not return maximum values [1., 18.] "
+        assert (scaler.minimum == np.array([-1., 2.])).all(), "scaler fit does not return maximum values [-1., 2.] " 
+        
+        
+    def test_min_max_scaler(self):
+        data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
+        expected = np.array([[0., 0.], [0.25, 0.25], [0.5, 0.5], [1., 1.]])
+        scaler = MinMaxScaler()
+        scaler.fit(data)
+        result = scaler.transform(data)
+        assert (result == expected).all(), "Scaler transform does not return expected values. All Values should be between 0 and 1. Got: {}".format(result.reshape(1,-1))
+        
+    def test_min_max_scaler_single_value(self):
+        data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
+        expected = np.array([[1.5, 0.]])
+        scaler = MinMaxScaler()
+        scaler.fit(data)
+        result = scaler.transform([[2., 2.]]) 
+        assert (result == expected).all(), "Scaler transform does not return expected values. Expect [[1.5 0. ]]. Got: {}".format(result)
+        
+    def test_standard_scaler_init(self):
+        scaler = StandardScaler()
+        assert isinstance(scaler, StandardScaler), "scaler is not a StandardScaler object"
+        
+    def test_standard_scaler_get_mean(self):
+        scaler = StandardScaler()
+        data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+        expected = np.array([0.5, 0.5])
+        scaler.fit(data)
+        assert (scaler.mean == expected).all(), "scaler fit does not return expected mean {}. Got {}".format(expected, scaler.mean)
+        
+    def test_standard_scaler_transform(self):
+        scaler = StandardScaler()
+        data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+        expected = np.array([[-1., -1.], [-1., -1.], [1., 1.], [1., 1.]])
+        scaler.fit(data)
+        result = scaler.transform(data)
+        assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
+        
+    def test_standard_scaler_single_value(self):
+        data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+        expected = np.array([[3., 3.]])
+        scaler = StandardScaler()
+        scaler.fit(data)
+        result = scaler.transform([[2., 2.]]) 
+        assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
+
+    # TODO: Add a test of your own below this line
+
+    def test_label_encoder_init(self):
+        encoder = LabelEncoder()
+        assert isinstance(encoder, LabelEncoder), "encoder is not a LabelEncoder object"
+    
+    def test_label_encoder_fit(self):
+        encoder = LabelEncoder()
+        data = [10, 20, 10, 30, 20, 40]
+        encoder.fit(data)
+        expected_classes = np.array([10, 20, 30, 40])
+        assert (encoder.classes_ == expected_classes).all(), "LabelEncoder fit does not return expected classes. Got: {}".format(encoder.classes_)
+    
+    def test_label_encoder_transform(self):
+        encoder = LabelEncoder()
+        data = [10, 20, 30, 40]
+        encoder.fit(data)
+        transformed = encoder.transform([10, 20, 30, 40])
+        expected = np.array([0, 1, 2, 3])
+        assert (transformed == expected).all(), "LabelEncoder transform does not return expected values. Expect {}. Got: {}".format(expected, transformed)
+    
+    def test_label_encoder_fit_transform(self):
+        encoder = LabelEncoder()
+        data = [100, 200, 300, 100, 200, 400]
+        transformed = encoder.fit_transform(data)
+        expected = np.array([0, 1, 2, 0, 1, 3])
+        assert (transformed == expected).all(), "LabelEncoder fit_transform does not return expected values. Expect {}. Got: {}".format(expected, transformed)
+    
+
+    
+if __name__ == '__main__':
+    unittest.main()
